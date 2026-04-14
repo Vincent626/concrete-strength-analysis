4b6765189e9fe8ce030a387170c912e861d3ca20
Administrator <admin@localhost>
2026-04-15 00:02:44 +0800

    Update model & dataset

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -5,11 +5,14 @@ echo ==============================================
 
 cd /d "d:\OneDrive - 振添股份有限公司\ML_DATA\steve_yu"
 
+echo [0/3] Setting remote URL...
+git remote set-url origin https://Vincent626@github.com/Vincent626/concrete-strength-analysis.git
+
 echo [1/3] Adding files...
 git add .
 
 echo [2/3] Committing changes...
-git commit -m "Update v8: Integrated mixer current features for M60 model improvement (R2=0.19)"
+git commit -m "Update model & dataset"
 
 echo [3/3] Pushing to remote...
 git push origin main
