c7533506d57e868e1a4bc32348f329eb491b29c2
Eddy Tang <eddyt@example.com>
2026-06-26 14:06:20 +0800

    Update dataset & front‑end files

--- commit_and_push.bat
+++ commit_and_push.bat
@@ -5,8 +5,15 @@ echo ==============================================
 
 cd /d "d:\OneDrive - 振添股份有限公司\ML_DATA\steve_yu"
 
+:: 若有 .env，載入其中的環境變數
+if exist ".env" (
+	for /f "usebackq tokens=1,* delims==" %%A in (".env") do (
+		set "%%A=%%B"
+	)
+)
+
 echo [0/3] Setting remote URL...
-git remote set-url origin https://Vincent626@github.com/Vincent626/concrete-strength-analysis.git
+git remote set-url origin https://%GIT_TOKEN%@github.com/Vincent626/concrete-strength-analysis.git
 
 echo [1/3] Adding files...
 git add .
